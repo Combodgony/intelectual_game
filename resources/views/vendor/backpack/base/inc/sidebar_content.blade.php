b8010e959ddcdacbdfd2204865f81d0023204675
Panasenco <panasenco@outlook.com>
2018-05-09 18:09:28 +0300

    add gamer table

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,4 +1,5 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
 <li>
     <a href="{{ backpack_url('dashboard') }}">
         <i class="fa fa-dashboard"></i>
